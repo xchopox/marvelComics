f8cd0fbbe50b034e49ae594fd3e05be1b615ecce
xchopox <felipe.lorca.galvez@gmail.com>
2015-06-09 11:18:09 -0300

    Creación del template principal
    Se añadió la barra de búsqueda, en template/comic-list.html, para que
    el usuario ingrese la query a realizar.

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+//angular.module('starter', ['ionic'])
+
+angular.module('marvelComics', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +19,17 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+
+.config(function ($stateProvider, $urlRouterProvider){
+
+	$stateProvider
+		.state('search', {
+			url:'/search',
+			templateUrl: 'templates/comic-list.html'
+		});
+
+	$urlRouterProvider.otherwise('/search');
+
+});
+
