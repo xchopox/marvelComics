730b8b4d06c5653a910777f5ee54c60a5bc7c89c
xchopox <felipe.lorca.galvez@gmail.com>
2015-06-09 19:06:34 -0300

    Carga de datos en la vista principal
    Se cargan los datos del personaje a buscar en la pantalla principal.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,7 +12,21 @@ angular.module('marvelComics.controllers',['marvelComics.services'])
 
 			Comics.findByQuery($scope.searchText).then(function(result){
 				var data = result.data.results[0];
+
+				$scope.heroName = data.name;
+				$scope.heroImg = data.thumbnail.path+'.'+data.thumbnail.extension;
+				$scope.attributionText = result.attributionText;
+
+				var desc = data.description;
+
+				if(desc.length <=0){
+					desc = 'No description for this character >:(';
+				}
+
+				$scope.heroDescription = desc;
+
 				console.log('El nombre es: '+ data.name);
+				console.log('Descripcion: '+ data.description);
 			});
 			
 		}
--- www/js/services.js
+++ www/js/services.js
@@ -8,7 +8,8 @@ angular.module('marvelComics.services', ['ngResource'])
 	//var date = new Date().getTime();
 	//var toHash = date+privateKey+publicKey;
 	//var hash = md5.createHash(toHash);
-	
+
+	// Para el hash md5(ts+privateKey+publicKey)	
 	var momentHash = '494bbae6ae425fc2b51840e400edfdcc';
 	var momentTs = '1433870299';
 	console.log(momentHash);
